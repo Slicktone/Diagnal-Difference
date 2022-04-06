e89585c15461199957db5daaba2f8a05196d8a4d
Slicktone <tclaytony23@gmail.com>
2022-04-05 18:29:39 -0700

    thought process

--- diagonal-diff.js
+++ diagonal-diff.js
@@ -1,3 +1,9 @@
 function diagonalDifference(arr) {
+    // thought process is to create 2 arrays with the left diagonal numbers and right diagonal numbers
+    // add the left diagonal
+    // add the right diagonal
+    // take the two sums and subtract them (absolute value)
+    // place this value as a single integer
+
     
 }
