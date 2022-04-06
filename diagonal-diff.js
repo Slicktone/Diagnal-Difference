55ef9dafbe3cdae2c65d09ea1db92a40791f3302
Slicktone <tclaytony23@gmail.com>
2022-04-05 18:34:21 -0700

    More notes

--- diagonal-diff.js
+++ diagonal-diff.js
@@ -5,5 +5,16 @@ function diagonalDifference(arr) {
     // take the two sums and subtract them (absolute value)
     // place this value as a single integer
 
-    
+    /* given input from challenge: 
+        11 2 4
+        4 5 6
+        10 8 -12
+
+        The left diagonal is 11, 5, -12
+        The right diagonal is 4, 5, 10
+
+        The sum of the left is 4, and the sum of the right is 15
+        The absolute value of both is |4 - 19| = 15
+        Math stuff: |x| is the absolute value of x
+    */
 }
